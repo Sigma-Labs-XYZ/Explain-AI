b8aaa3b15a0a7e3e3dc5932184e27287f749fa21
Yuna Iwata <yunaiwata@Yunas-MacBook-Pro.local>
2023-02-20 15:35:35 +0000

    yuna test commit

--- web/src/App.js
+++ web/src/App.js
@@ -9,7 +9,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Hello Yuna!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
