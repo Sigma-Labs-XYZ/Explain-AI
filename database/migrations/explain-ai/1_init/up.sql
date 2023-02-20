f99de2cf4c144702543b95209917a0480b29ed1d
Dom Vinyard <domvinyard@me.com>
2023-02-20 08:23:29 +0000

    Update up.sql

--- database/migrations/explain-ai/1_init/up.sql
+++ database/migrations/explain-ai/1_init/up.sql
@@ -11,6 +11,7 @@ CREATE TABLE public.relationship (
     from_slug text NOT NULL,
     to_slug text NOT NULL,
     description text NOT NULL,
+    audience numeric DEFAULT 10 NOT NULL,
     priority integer DEFAULT 0 NOT NULL
 );
 CREATE TABLE public.topic (
@@ -36,9 +37,9 @@ INSERT INTO public.topic (slug, image, name) VALUES ('javascript', NULL, 'Javasc
 INSERT INTO public.topic (slug, image, name) VALUES ('html', NULL, 'HTML');
 INSERT INTO public.topic (slug, image, name) VALUES ('css', NULL, 'CSS');
 INSERT INTO public.topic (slug, image, name) VALUES ('react', NULL, 'React');
-INSERT INTO public.description (id, topic_slug, audience, short, long) VALUES ('828cbb0a-8105-4ce6-950c-6ed1e6aad524', 'javascript', 5, 'JavaScript is a special language that helps make websites more fun and exciting. It helps your computer do things like move things around on the screen, make things happen when you click on them, and make things look different.', NULL);
-INSERT INTO public.description (id, topic_slug, audience, short, long) VALUES ('515c491f-a904-4a89-83e9-49bea4e801cd', 'javascript', 10, 'JavaScript is a type of computer code that helps webpages do cool things. For example, when you play a game on a website, that game is powered by JavaScript. It can also make webpages look nicer by adding colors and animations. JavaScript is like a secret language that computers understand, and it helps make the internet a fun and exciting place.', NULL);
-INSERT INTO public.description (id, topic_slug, audience, short, long) VALUES ('94569333-40a6-46f0-9e7f-aa0bb57bc692', 'javascript', 20, 'JavaScript is a computer programming language that is used to create interactive websites and applications. It allows web developers to create websites and applications that respond to user actions, such as moving a mouse over a button, typing something in a textbox, or clicking a link. It is used to add behavior, store and retrieve information, and change the style and layout of web pages.', NULL);
-INSERT INTO public.relationship (id, from_slug, to_slug, description, priority) VALUES ('eb72a094-0376-4ce9-86ff-fcc9386c65db', 'javascript', 'css', 'Javascript and CSS work together to make websites look good and function properly.', 0);
-INSERT INTO public.relationship (id, from_slug, to_slug, description, priority) VALUES ('6313a828-8861-4180-a5ae-0fa00c6d3be5', 'html', 'css', 'JavaScript is used to add interactivity to HTML pages.', 0);
+INSERT INTO public.description (topic_slug, audience, short, long) VALUES ('javascript', 5, 'JavaScript is a special language that helps make websites more fun and exciting. It helps your computer do things like move things around on the screen, make things happen when you click on them, and make things look different.', NULL);
+INSERT INTO public.description (topic_slug, audience, short, long) VALUES ('javascript', 10, 'JavaScript is a type of computer code that helps webpages do cool things. For example, when you play a game on a website, that game is powered by JavaScript. It can also make webpages look nicer by adding colors and animations. JavaScript is like a secret language that computers understand, and it helps make the internet a fun and exciting place.', NULL);
+INSERT INTO public.description (topic_slug, audience, short, long) VALUES ('javascript', 20, 'JavaScript is a computer programming language that is used to create interactive websites and applications. It allows web developers to create websites and applications that respond to user actions, such as moving a mouse over a button, typing something in a textbox, or clicking a link. It is used to add behavior, store and retrieve information, and change the style and layout of web pages.', NULL);
+INSERT INTO public.relationship (from_slug, to_slug, description, priority) VALUES ('javascript', 'css', 'Javascript and CSS work together to make websites look good and function properly.', 0);
+INSERT INTO public.relationship (from_slug, to_slug, description, priority) VALUES ('html', 'css', 'JavaScript is used to add interactivity to HTML pages.', 0);
 
