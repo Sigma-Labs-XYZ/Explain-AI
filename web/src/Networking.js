7f5794e7b435dd6f916bad3aa01c31b0296755cf
Yuna Iwata <yunaiwata@Yunas-MacBook-Pro.local>
2023-02-20 16:08:24 +0000

    fetched topc data and displayed on main page

---
+++ web/src/Networking.js
@@ -0,0 +1,11 @@
+const baseUrl = "http://localhost:4000";
+
+export async function fetchTopics(setTopics) {
+  try {
+    const data = await fetch(`${baseUrl}/topics`);
+    const json = await data.json();
+    setTopics(JSON.stringify(json));
+  } catch (e) {
+    console.log(e);
+  }
+}
