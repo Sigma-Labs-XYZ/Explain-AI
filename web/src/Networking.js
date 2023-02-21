5c5b3f258ae8e967e71be02b67e9f8d9d4fc9a66
Swastik Dilip <justsyd224@gmail.com>
2023-02-21 11:17:31 +0000

    Got rid of commented code in networking

--- web/src/Networking.js
+++ web/src/Networking.js
@@ -1,10 +1,9 @@
 const baseUrl = "http://localhost:4000";
 
-export async function fetchTopics(setTopics) {
+export async function fetchTopics() {
   try {
     const data = await fetch(`${baseUrl}/topics`);
     const json = await data.json();
-    // setTopics(JSON.stringify(json));
     return json;
   } catch (e) {
     console.log(e);
