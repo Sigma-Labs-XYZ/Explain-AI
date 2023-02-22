c6d90e8cc5f0bc39972e3f1650dfc80f1a3a10a1
Ademsk1 <adam.illoul24@gmail.com>
2023-02-22 13:14:28 +0000

    changing name of App-> AppRouter

--- web/src/index.js
+++ web/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './AppRouter';
+import AppRouter from './AppRouter';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom'
 
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+    <AppRouter />
     </BrowserRouter>
   </React.StrictMode>
 );
