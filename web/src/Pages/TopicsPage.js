b09e797416009e5ce9dd0d218823442c53ad846d
Mariam-mohamed110 <mariammohamed0313@gmail.com>
2023-02-22 13:11:52 +0000

    link added

--- web/src/Pages/TopicsPage.js
+++ web/src/Pages/TopicsPage.js
@@ -1,5 +1,14 @@
 import FetchDataDiv from "../Components/fetchDataDiv";
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function TopicsPage({retrievedTopics}) {
-  return <FetchDataDiv retrievedTopics={retrievedTopics}/>;
+export default function TopicsPage({ retrievedTopics }) {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Homepage</Link>
+      </nav>
+      <FetchDataDiv retrievedTopics={retrievedTopics} />
+    </div>
+  );
 }
