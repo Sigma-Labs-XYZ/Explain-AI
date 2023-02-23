10239f7c382e24ed8d85bbda0d2f5b1f7051cefa
bp289 <birajpanta112@gamil.com>
2023-02-23 16:12:32 +0000

    found info about resizing, but not finished tests

--- web/src/Tests/Header.test.js
+++ web/src/Tests/Header.test.js
@@ -1,4 +1,5 @@
 import { render, screen, cleanup, getByText } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Header from "../components/Header";
 
 describe("Testing Nav Bar", () => {
@@ -20,7 +21,51 @@ describe("Testing Nav Bar", () => {
     expect(optionAdult).toHaveClass("unselected");
   });
 
-  test("testing localStorage Persistance ", () => {});
+  test("testing localStorage Persistance ", () => {
+    Object.defineProperty(window, "localStorage", {
+      value: {
+        getItem: jest.fn(() => null),
+        setItem: jest.fn(() => null),
+      },
+      writable: true,
+    });
+    render(<Header />);
+    expect(window.localStorage.getItem).toBeCalledTimes(1);
 
-  test("testing responsive design", () => {});
+    const optionTen = screen.getByTestId("ten");
+
+    userEvent.click(optionTen);
+
+    expect(window.localStorage.setItem).toBeCalledTimes(1);
+    expect(window.localStorage.setItem).toHaveBeenCalledWith(
+      "selectedExplanation",
+      "10"
+    );
+  });
+
+  test("testing responsive design", () => {
+    render(<Header />);
+
+    const resizeWindow = (x, y) => {
+      window.innerWidth = x;
+      window.innerHeight = y;
+      window.dispatchEvent(new Event("resize"));
+    };
+    screen.debug();
+
+    resizeWindow(600, 812); // we want there to be a selector
+    const buttons = screen.queryByTestId("selectorButtons", { hidden: true });
+    const buttonSelector = screen.queryAllByRole("button");
+    expect(window.innerWidth).toEqual(600);
+    expect(buttons).toBeInTheDocument();
+    expect(buttonSelector.length).toEqual(3);
+
+    resizeWindow(550, 812); //we want there to be a drop down
+    const dropDown = screen.getByTestId("dropdown");
+    const dropDownSelector = screen.queryAllByRole("option");
+    expect(window.innerWidth).toEqual(550);
+    expect(buttons).not.toBeVisible();
+    expect(dropDown).toBeInTheDocument();
+    expect(dropDownSelector.length).toEqual(3);
+  });
 });
--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -1,18 +1,21 @@
 import { useEffect, useState } from "react";
 import styles from "../Styles/styles.css";
 import logo from "../logo.png";
+
+//change audience to selectedAudience
 export default function Header() {
-  const [selectedExplanation, setSelectedExplanation] = useState("fiveStyle");
+  const [audience, setAudience] = useState("5");
 
   function select(name) {
-    setSelectedExplanation(name);
+    setAudience(name);
+    // console.log(audience);
     localStorage.setItem("selectedExplanation", name);
   }
 
   useEffect(() => {
     const current = localStorage.getItem("selectedExplanation");
     if (current) {
-      setSelectedExplanation(current);
+      setAudience(current);
     }
   }, []);
   return (
@@ -21,48 +24,49 @@ export default function Header() {
         <img className="logo" src={logo} alt="Explain AI"></img>
         <div className="selector">
           <p className="textStyle"> Like I'm </p>
-          <button
-            data-testid="five"
-            className={
-              selectedExplanation === "fiveStyle" ? "selected" : "unselected"
-            }
-            name="fiveStyle"
-            onClick={(event) => {
-              select(event.target.name);
-              console.log(event.target.name);
-            }}
-          >
-            5
-          </button>
-          <button
-            data-testid="ten"
-            className={
-              selectedExplanation === "tenStyle" ? "selected" : "unselected"
-            }
-            name="tenStyle"
-            onClick={(event) => {
-              select(event.target.name);
-              console.log(event.target.name);
-            }}
-          >
-            10
-          </button>
-          <button
-            data-testid="adult"
-            className={
-              selectedExplanation === "adultStyle" ? "selected" : "unselected"
-            }
-            name="adultStyle"
-            onClick={(event) => {
-              select(event.target.name);
-              console.log(event.target.name);
-            }}
-          >
-            Adult
-          </button>
-          <div className="dropdown">
+          <div data-testid="selectorButtons" className="selectorButtons">
+            <button
+              data-testid="five"
+              className={audience === "5" ? "selected" : "unselected"}
+              name="5"
+              onClick={(event) => {
+                select(event.target.name);
+              }}
+            >
+              5
+            </button>
+            <button
+              data-testid="ten"
+              className={audience === "10" ? "selected" : "unselected"}
+              name="10"
+              onClick={(event) => {
+                select(event.target.name);
+              }}
+            >
+              10
+            </button>
+            <button
+              data-testid="adult"
+              className={audience === "Adult" ? "selected" : "unselected"}
+              name="Adult"
+              onClick={(event) => {
+                select(event.target.name);
+              }}
+            >
+              Adult
+            </button>
+          </div>
+          <div data-testid="dropdown" className="dropdown">
             <form action="/action_page.php">
-              <select name="explainLevels" id="explainLevels">
+              <select
+                value={audience}
+                name="explainLevels"
+                id="explainLevels"
+                onChange={(event) => {
+                  console.log(event.target.value);
+                  select(event.target.value);
+                }}
+              >
                 <option value="5">5</option>
                 <option value="10">10</option>
                 <option value="Adult">Adult</option>
