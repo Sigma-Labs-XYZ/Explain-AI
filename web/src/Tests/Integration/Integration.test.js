cb5210f772c0c1ae57a2a62fac5d6e36af9a4950
Ademsk1 <adam.illoul24@gmail.com>
2023-02-23 14:23:15 +0000

    fixed integration test

--- web/src/Tests/Integration/Integration.test.js
+++ web/src/Tests/Integration/Integration.test.js
@@ -1,20 +1,21 @@
 import { render, screen, cleanup, getByText } from "@testing-library/react";
 import App from "../../App.js";
+import AppRouter from "../../AppRouter.js";
 import { waitFor } from "@testing-library/react";
 import userEvent from '@testing-library/user-event'
+import {MemoryRouter} from 'react-router-dom'
 
 test("check to make sure api response is accurate by checking correct keys", async () => {
   
-    render(<App/>)
+    render(<AppRouter/>,{wrapper:MemoryRouter})
 
     const links = screen.getAllByRole('link')
+
     for (let link of links) {
-      console.log(link.textContent)
-      await userEvent.click(link) //go to a topic page
+      render(<AppRouter/>,{wrapper:MemoryRouter})
+      userEvent.click(link) //go to a topic page
+      //const stuff = await screen.findByRole('navigation')
       const topicInfo = await screen.findByTestId('jsondat') //find test-id
       expect(topicInfo).toBeInTheDocument()
-      const linkBack = screen.getByRole('link')
-      await userEvent.click(linkBack) //sends user back to homepage
-      setTimeout(()=>{},1000)
     }
 });
