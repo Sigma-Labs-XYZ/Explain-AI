96e5ab8aded4db6715298571c0833c6677cb4a52
Dom Vinyard <domvinyard@me.com>
2023-02-24 14:32:18 +0000

    Update App.js

--- web/src/App.js
+++ web/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import { fetchData } from "./utils/networking";
 
-const MAIN_URL = "http://localhost:4000/topics";
+const MAIN_URL = `${process.env.REACT_APP_API_ENDPOINT}/topics`;
 function App() {
   const [retrievedTopics, setRetrievedTopics] = useState();
 
