8cc73266ceb1a3507c7d9a3eaab01dd67df0b19d
Ademsk1 <adam.illoul24@gmail.com>
2023-02-25 15:16:57 +0000

    added in topic card styling and adjusting image position for screen-size changes

--- web/src/components/TopicCard.js
+++ web/src/components/TopicCard.js
@@ -1,4 +1,4 @@
-
+import '../Styling/TopicCard.css'
 function findShortDescription(descriptions,audience) {
     return descriptions.filter(description=> {
         return description.audience===audience
@@ -7,17 +7,30 @@ function findShortDescription(descriptions,audience) {
 
 
 export function TopicCard({topic}) {
-    const audience = 5 //this will be passed through as a prop later on from the Header
+    const audience = 20//this will be passed through as a prop later on from the Header
     const description = findShortDescription(topic.descriptions,audience)
     console.log(description)
     return (
-        <div className="topic-card">
-            <h1>
-                {topic.name}
-            </h1>
-            <p>
-                {description}
-            </p>
+        <div className="topic-card-parent">
+            <div className='topic-card'>
+                <div className='topic-card-title'>
+                    <h1>
+                        {topic.name}
+                    
+                    </h1>
+                </div>
+                <div className='topic-card-description'>
+                    <p>
+                        {description}
+                    </p>
+                </div>
+                <div className='tell-more-button'>
+                    <button>
+                        Tell me more
+                    </button>
+                </div>
+                <img src={topic.image} alt={`A representation of ${topic.name}`}/>
+            </div>
 
         </div>
     )
