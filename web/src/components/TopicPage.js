49808c053ccf71c79685203ea8a6bab855da0b77
Mariam-mohamed110 <mariammohamed0313@gmail.com>
2023-02-27 14:13:21 +0000

    styling completed

--- web/src/components/TopicPage.js
+++ web/src/components/TopicPage.js
@@ -3,30 +3,24 @@ import { useState, useEffect } from "react";
 import { fetchData } from "../utils/networking";
 import { TopicCard } from "./TopicCard";
 
-
 export default function TopicPage() {
   const { topic } = useParams();
   const [retrievedTopics, setRetrievedTopics] = useState();
   const MAIN_URL = `${process.env.REACT_APP_API_ENDPOINT}/topic/${topic}`;
   useEffect(() => {
     (async function () {
-      const fetchedData = await fetchData(MAIN_URL)
+      const fetchedData = await fetchData(MAIN_URL);
       if (fetchedData) {
-        setRetrievedTopics(fetchedData)
-      }
-      else {
-        setRetrievedTopics(undefined)
+        setRetrievedTopics(fetchedData);
+      } else {
+        setRetrievedTopics(undefined);
       }
     })();
   }, [MAIN_URL]);
-  
+
   return (
     <div>
-
-      {retrievedTopics && (
-        <TopicCard topic={retrievedTopics.topic[0]}/>
-      )}
+      {retrievedTopics && <TopicCard topic={retrievedTopics.topic[0]} />}
     </div>
   );
 }
-
