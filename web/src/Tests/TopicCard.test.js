d2b178d2ede7fcc8dfd275e18ffdf2e0fa57f318
Ademsk1 <adam.illoul24@gmail.com>
2023-02-27 13:04:14 +0000

    adding in dummy data and no-image file

--- web/src/Tests/TopicCard.test.js
+++ web/src/Tests/TopicCard.test.js
@@ -1,10 +1,10 @@
 import {render,screen} from '@testing-library/react'
+import { TopicCard } from '../components/TopicCard'
 
-const dummy_info = {
+const test_data = await (await fetch('./dummy_data.json')).json()
 
-}
 describe('Tests for TopicCard',()=> {
     test('Renders information correctly',()=> {
-        
+        render(<TopicCard />)
     })
 })
