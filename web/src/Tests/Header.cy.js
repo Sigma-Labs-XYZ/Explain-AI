90129a254d25f43f3af0ae1a86a45ea464c55494
s-dilip <justsyd224@gmail.com>
2023-02-27 11:25:11 +0000

    Media Queries age selector tests

--- web/src/Tests/Header.cy.js
+++ web/src/Tests/Header.cy.js
@@ -1,10 +1,31 @@
 import Header from "../components/Header";
 import React from "react";
 
-it("Header displays Age Toggle if screen size above 400px", () => {
-  //   cy.viewport("iphone-5");
-  cy.viewport("macbook-15");
+describe("Media queries and their effects on Age Toggle and Dropdown", () => {
+  //Checking Toggle Appears when screen above 400px
+  it("Toggle appears when screen width above 400px", () => {
+    cy.viewport(405, 1000);
+    cy.mount(<Header />);
+    cy.get("button").should("not.have.css", "display", "none");
+  });
 
-  cy.mount(<Header />);
-  cy.get("button").should("not.have.css", "display", "none");
+  it("Toggle disappears when screen width below 400px", () => {
+    cy.viewport(395, 1000);
+    cy.mount(<Header />);
+    cy.get("button").should("have.css", "display", "none");
+  });
+
+  //Checking Dropdown appears when screen below 400px
+  it("Dropdown appears when screen width below 400px", () => {
+    cy.viewport(395, 1000);
+    cy.mount(<Header />);
+    cy.get("select").should("not.have.css", "display", "none");
+  });
+
+  //Checking Toggle disappears when screen below 400px
+  it("Dropdown appears when screen width below 400px", () => {
+    cy.viewport(405, 1000);
+    cy.mount(<Header />);
+    cy.get("select").should("have.css", "display", "none");
+  });
 });
