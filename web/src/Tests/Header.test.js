f23d89808e64b076d28095e5f4ff9f73911b8587
bp289 <birajpanta112@gamil.com>
2023-02-27 17:53:38 +0000

    testing fixed

--- web/src/Tests/Header.test.js
+++ web/src/Tests/Header.test.js
@@ -1,24 +1,24 @@
 import { render, screen, cleanup, getByText } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import Header from "../components/Header";
+import Header from "../components/Header/Header";
 
 describe("Testing Nav Bar", () => {
   test("testing components appear on screen", () => {
     render(<Header />);
     const logo = screen.getByAltText("Explain AI");
     const optionLabel = screen.getByText("Like I'm");
-    const optionFive = screen.getByTestId("five");
-    const optionTen = screen.getByTestId("ten");
-    const optionAdult = screen.getByTestId("adult");
-
+    const labels = ["five", "ten", "adult"];
+    const options = labels.map((label) => {
+      return screen.getByTestId(label);
+    });
+    options.forEach((option) => {
+      expect(option).toBeInTheDocument();
+    });
     expect(logo).toBeInTheDocument();
     expect(optionLabel).toBeInTheDocument();
-    expect(optionFive).toBeInTheDocument();
-    expect(optionTen).toBeInTheDocument();
-    expect(optionAdult).toBeInTheDocument();
-    expect(optionFive).toHaveClass("selected");
-    expect(optionTen).toHaveClass("unselected");
-    expect(optionAdult).toHaveClass("unselected");
+    expect(options[0]).toHaveClass("selected");
+    expect(options[1]).toHaveClass("unselected");
+    expect(options[2]).toHaveClass("unselected");
   });
 
   test("testing localStorage Persistance ", () => {
