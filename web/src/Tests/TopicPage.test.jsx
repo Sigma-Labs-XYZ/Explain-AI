5585fa247f28b25372ab57e4a93914c777f480e6
mdapim <mdapim@outlook.com>
2023-02-27 15:33:50 +0000

    added eslint, including testing, jest plugins and refactorred code

--- web/src/Tests/TopicPage.test.jsx
+++ web/src/Tests/TopicPage.test.jsx
@@ -1,7 +1,8 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import fetchMock from "jest-fetch-mock";
 import React from "react";
-import TopicPage from "../components/TopicPage";
+import App from "../App";
+
 
 beforeEach(() => {
   fetchMock.enableMocks();
@@ -9,11 +10,12 @@ beforeEach(() => {
 });
 
 describe("Test for API render on sucess", () => {
-  test("Check that a fetch is made", async () => {
+  test("a p tag is created with json file upon a successful fetch", async () => {
     fetch.mockResponseOnce(JSON.stringify({ test: "test" }));
 
-    render(<TopicPage />);
+    render(<App />);
     expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith("http://localhost:4000/topics");
     await waitFor(() => {
       expect(screen.getByText('{"test":"test"}')).toBeInTheDocument();
     });
@@ -22,8 +24,10 @@ describe("Test for API render on sucess", () => {
   // created for testing unsuccessfull requests in the future
   test("a p tag is not created with json file upon an unsuccessful fetch", async () => {
     fetch.mockResponseOnce(JSON.stringify());
-    render(<TopicPage />);
+
+    render(<App />);
     expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith("http://localhost:4000/topics");
     await waitFor(() => {
       expect(screen.queryByTestId("jsondat")).not.toBeInTheDocument();
     });
