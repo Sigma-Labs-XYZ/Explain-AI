7d5ee72a9fcc9e7bc3680e40e1c276e67437e665
s-dilip <justsyd224@gmail.com>
2023-02-27 11:27:04 +0000

    Fixed comments and test desc

--- web/src/Tests/Header.cy.js
+++ web/src/Tests/Header.cy.js
@@ -2,7 +2,6 @@ import Header from "../components/Header";
 import React from "react";
 
 describe("Media queries and their effects on Age Toggle and Dropdown", () => {
-  //Checking Toggle Appears when screen above 400px
   it("Toggle appears when screen width above 400px", () => {
     cy.viewport(405, 1000);
     cy.mount(<Header />);
@@ -15,15 +14,13 @@ describe("Media queries and their effects on Age Toggle and Dropdown", () => {
     cy.get("button").should("have.css", "display", "none");
   });
 
-  //Checking Dropdown appears when screen below 400px
   it("Dropdown appears when screen width below 400px", () => {
     cy.viewport(395, 1000);
     cy.mount(<Header />);
     cy.get("select").should("not.have.css", "display", "none");
   });
 
-  //Checking Toggle disappears when screen below 400px
-  it("Dropdown appears when screen width below 400px", () => {
+  it("Dropdown disappears when screen width above 400px", () => {
     cy.viewport(405, 1000);
     cy.mount(<Header />);
     cy.get("select").should("have.css", "display", "none");
