2010dbe6e72fc2f53d16d0e99543ddc494330dd2
Ademsk1 <adam.illoul24@gmail.com>
2023-02-27 13:15:36 +0000

    tests

--- web/src/Tests/TopicCard.test.js
+++ web/src/Tests/TopicCard.test.js
@@ -1,10 +1,14 @@
 import {render,screen} from '@testing-library/react'
 import { TopicCard } from '../components/TopicCard'
-
-const test_data = await (await fetch('./dummy_data.json')).json()
+import test_data from './dummy_data.json'
+const topic = test_data.topic[0]
 
 describe('Tests for TopicCard',()=> {
     test('Renders information correctly',()=> {
-        render(<TopicCard />)
+        render(<TopicCard topic={topic}/>)
+        const title = screen.getByRole('heading')
+        const description = screen.getByRole('')
+        expect(title.textContent).toBe(topic.name)
+
     })
 })
