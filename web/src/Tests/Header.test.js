a9fe6fa1b9c0e8a3e7c45fc40fc13897c3df0cbd
s-dilip <93096031+s-dilip@users.noreply.github.com>
2023-02-28 15:17:17 +0000

    Yuna Swastik Header (#30)
    * styled logo in header
    
    * buttons styled
    
    * styled dropdown
    
    Co-authored-by: s-dilip <s-dilip@users.noreply.github.com>
    
    * test commit
    
    * add conditional classes
    
    * local storage added
    
    * local storage and toggle tests
    
    * local storage tests done
    
    * logo test
    
    * Cypress Running
    
    * test elements of header
    
    * Media Queries age selector tests
    
    * Fixed comments and test desc
    
    * Added adult font size change test
    
    * cleared up tests
    
    * Added test for button bg change when clicked
    
    * Added Margin Test for superwide
    
    * More descriptive Test Names
    
    * Snapshot deleted
    
    * Integration test deleted
    
    * Database Commit
    
    * Added setting default age to 5
    
    Co-Authored-By: yuna-iwata <115478706+yuna-iwata@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Yuna Iwata <yunaiwata@Yunas-MacBook-Pro.local>
    Co-authored-by: yuna <yuna.iwata@sigmalabs.co.uk>
    Co-authored-by: s-dilip <s-dilip@users.noreply.github.com>
    Co-authored-by: yuna-iwata <115478706+yuna-iwata@users.noreply.github.com>

---
+++ web/src/Tests/Header.test.js
@@ -0,0 +1,83 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import React from "react";
+import renderer from "react-test-renderer";
+import userEvent from "@testing-library/user-event";
+import Header from "../components/Header";
+
+describe("localStorage tests", () => {
+  beforeEach(() => {
+    localStorage.clear();
+    jest.clearAllMocks();
+  });
+  const key = "age";
+
+  test("should save to local storage", () => {
+    const value = "5";
+    localStorage.setItem(key, value);
+    expect(localStorage.setItem).toHaveBeenLastCalledWith(key, value);
+    expect(localStorage.__STORE__[key]).toBe(value);
+    expect(Object.keys(localStorage.__STORE__).length).toBe(1);
+  });
+
+  test("correct option should be selected when localStorage is set", () => {
+    const value = "10";
+    localStorage.setItem(key, value);
+    render(<Header />);
+    const options = screen.getAllByRole("option");
+    expect(options[0].selected).toBeFalsy();
+    expect(options[1].selected).toBeTruthy();
+    expect(options[2].selected).toBeFalsy();
+  });
+
+  test("when age button is clicked it should update localStorage with the correct value", () => {
+    render(<Header />);
+    const value = "Adult";
+    const button = screen.getByRole("button", {
+      name: value,
+    });
+    fireEvent.click(button);
+    expect(localStorage.setItem).toHaveBeenLastCalledWith(key, "Adult");
+    expect(localStorage.__STORE__[key]).toBe(value);
+  });
+
+  test("when dropdown is changed it should update localStorage with the correct value", () => {
+    render(<Header />);
+    const value = "10";
+    const select = screen.getByRole("combobox");
+    const option = screen.getByRole("option", { name: value });
+    userEvent.selectOptions(select, option);
+    expect(localStorage.setItem).toHaveBeenLastCalledWith(key, value);
+    expect(localStorage.__STORE__[key]).toBe(value);
+  });
+});
+
+describe("test if all elements of header are rendered", () => {
+  const tags = ["5", "10", "Adult"];
+  test("logo rendered", () => {
+    render(<Header />);
+    const logo = screen.getByAltText("logo");
+    expect(logo.src).toContain("explainai-logo.png");
+  });
+
+  test("'Like I'm' <p> appears", () => {
+    render(<Header />);
+    expect(screen.getByText("Like I'm")).toBeInTheDocument();
+  });
+
+  test("buttons rendered", () => {
+    render(<Header />);
+    const buttons = screen.getAllByRole("button");
+    expect(buttons).toHaveLength(3);
+    tags.map((tag) => {
+      expect(screen.getByRole("button", { name: tag })).toBeInTheDocument();
+    });
+  });
+
+  test("dropdown rendered", () => {
+    render(<Header />);
+    expect(screen.getByRole("combobox")).toBeInTheDocument();
+    tags.map((tag) => {
+      expect(screen.getByRole("option", { name: tag })).toBeInTheDocument();
+    });
+  });
+});
