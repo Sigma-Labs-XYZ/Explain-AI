51632b1ce78e6195aed9dcb84f2f85e9c9fe6e11
Ademsk1 <85499621+Ademsk1@users.noreply.github.com>
2023-02-28 14:59:02 +0000

    error wiggle for topicCard

--- web/src/components/TopicCard.js
+++ web/src/components/TopicCard.js
@@ -3,13 +3,12 @@ import {useState} from 'react'
 
 
 export function TopicCard({topic,audience}) {
-    console.log({audience})
     const [error,setError] = useState(false)
-    const  findShortDescription = (descriptions,audience)=> {
+    const  getDescription = (descriptions,audience,length)=> {
         try {
-        return descriptions.filter(description=> {
-            return description.audience===audience
-        }).map(des=>des.short)
+            return descriptions.filter(description=> {
+                return description.audience===audience
+            }).map(des=>des.short)
         }
         catch {
             setError(true)
@@ -19,42 +18,33 @@ export function TopicCard({topic,audience}) {
         const imageError = typeof topic.image !=='string' || topic.image===''//this may be changed depending on how the API responds
         return (imageError? './no-image.jpeg' : topic.image)
     }
-    const descriptionHandler = ()=> {
-        if (error) {
-            return (
-                <div data-testid='error-api' className='error'>
-                    <h1>Error</h1>
-                    <h2>Something went wrong on our side!</h2>
-                </div>
-            )
-        }
-        else {
-            return findShortDescription(topic.descriptions,audience)
-        }
-    }
-    return (
-        <div className="topic-card-parent">
-            <div className='topic-card'>
-                <div className='topic-card-title'>
-                    <h1>
-                        {topic.name}
-                    </h1>
-                </div>
-                <div className='topic-card-description'>
-                    <p>
-                        {descriptionHandler()}
-                    </p>
-                </div>
-                <div className='topic-card-img-btn'>
-                    <button>
-                        Tell me more
-                    </button>
-                    <div className = 'topic-card-img'>
-                    <img src={imageHandler()} alt={`A representation of ${topic.name}`}/>
+    if (!error) {
+        return (
+            <div className="topic-card-parent">
+                <div className='topic-card'>
+                    <div className='topic-card-title'>
+                        <h1>{topic.name}</h1>
                     </div>
-            </div>
-            </div>
+                    <div className='topic-card-description'>
+                        <p> {getDescription(topic.descriptions,audience,'short')} </p>
+                    </div>
+                    <div className='topic-card-img-btn'>
+                        <button>Tell me more</button>
+                        <div className = 'topic-card-img'>
+                        <img src={imageHandler()} alt={`A representation of ${topic.name}`}/>
+                        </div>
+                    </div>
+                </div>
 
-        </div>
-    )
+            </div>
+        )}
+
+    else {
+        return (
+            <div data-testid='error-api' className='api-error'>
+                <h1>Error</h1>
+                <h2>The API format of this particular element is unreadable.</h2>
+            </div>
+        )
+    }
 }
--- web/src/components/TopicPage.js
+++ web/src/components/TopicPage.js
@@ -6,10 +6,19 @@ import { TopicCard } from "./TopicCard";
 export default function TopicPage() {
   const { topic } = useParams();
   const [retrievedTopics, setRetrievedTopics] = useState();
-  const [audience,] = useState(parseInt(localStorage.getItem('age') || localStorage.setItem('age',5) || 5))
-  
+  const [audience,setAudience] = useState()
+  useEffect(()=> {
+    const storedAge = localStorage.getItem('age')
+    if (storedAge) {
+      setAudience(parseInt(localStorage.getItem('age')))
+    }
+    else {
+      //defaults to 5
+      localStorage.setItem('age',5)
+      setAudience(5)
+    }
+  },[audience])
   const MAIN_URL = `${process.env.REACT_APP_API_ENDPOINT}/topic/${topic}`;
-  console.log(audience)
   useEffect(() => {
     (async function () {
       const fetchedData = await fetchData(MAIN_URL);
