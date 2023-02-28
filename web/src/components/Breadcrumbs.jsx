462e04fee06d6b4be08f51eebf65aedb0049284f
bp289 <birajpanta112@gamil.com>
2023-02-28 17:05:16 +0000

    data-test-ids added

--- web/src/components/Breadcrumbs.jsx
+++ web/src/components/Breadcrumbs.jsx
@@ -5,23 +5,34 @@ import "../App.css";
 export default function Breadcrumbs({ grandParent, parent }) {
   return (
     <>
-      {/* <nav>
+      <nav data-testid="desktop">
         <span className="mr-120 w-screen bg-backdrop text-white capitalize">
           <Link to={`/${grandParent.slug}`}>{grandParent.name} </Link>
-          <span className="font-thin"> > </span>
+          <span data-testid="desktop-grandParent" className="font-thin">
+            >
+          </span>
           <Link to={`/${parent.slug}`}>{parent.name} </Link>
-          <span className="font-thin"> > </span>
+          <span data-testid="desktop-parent" className="font-thin">
+            {" "}
+            >{" "}
+          </span>
         </span>
-      </nav> */}
+      </nav>
 
-      <div id="mobile" className="text-white capitalize ">
-        <div className="bg-backdrop2 pb-2 pt-2 text-left">
+      <div id="mobile" data-testid="mobile" className="text-white capitalize ">
+        <div
+          data-testid="mobile-grandparent"
+          className="bg-backdrop2 pb-2 pt-2 text-left"
+        >
           <Link className="ml-7" to={`/${grandParent.slug}`}>
             {grandParent.name}
           </Link>
           <span className="font-thin"> > </span>
         </div>
-        <div className="bg-backdrop pb-2 pt-2 text-left ">
+        <div
+          data-testid="mobile-parent"
+          className="bg-backdrop pb-2 pt-2 text-left "
+        >
           <Link className="ml-7" to={`/${parent.slug}`}>
             {parent.name}
           </Link>
