c2d3d01a8652fdad31a5ea56985ef4dc71843a96
yuna-iwata <yuna.iwata@sigmalabs.co.uk>
2023-02-28 18:01:08 +0000

    cypress tests

--- web/src/Tests/RelationCard.cy.js
+++ web/src/Tests/RelationCard.cy.js
@@ -1,4 +1,36 @@
 import RelationCard from "../components/RelationCard";
 import React from "react";
+import "../components/RelationCard.css";
 
-test("there are margins for desktop screensize", () => {});
+describe("media queries for the card display for phone/desktop", () => {
+  it("there are margins for desktop screen size", () => {
+    cy.viewport(500, 1000);
+    cy.mount(<RelationCard name="daddy" description="daddy" />);
+    cy.get('[data-test-id="card container"]').should(
+      "have.css",
+      "margin-left",
+      "20px"
+    );
+  });
+
+  it("there are no margins for phone", () => {
+    cy.viewport(400, 1000);
+    cy.mount(<RelationCard name="daddy" description="daddy" />);
+    cy.get('[data-test-id="card container"]').should(
+      "not.have.css",
+      "margin-left",
+      "20px"
+    );
+  });
+
+  it("image should be centred on the card container edge for desktop", () => {
+    cy.viewport(500, 1000);
+    cy.mount(<RelationCard name="daddy" description="daddy" />);
+    cy.get('[data-test-id="img container"]').should(
+      "have.css",
+      "margin-left",
+      "-48px"
+    );
+    cy.get("img").should("have.css", "width", "96px");
+  });
+});
--- web/src/components/RelationCard.js
+++ web/src/components/RelationCard.js
@@ -16,7 +16,10 @@ export default function RelationCard({ name, description }) {
           <p className="mt-2">{description}</p>
         </div>
       </div>
-      <div className="-ml-12 mr-5 mb-[0.8%] phone:-ml-32 phone:mr-8">
+      <div
+        className="-ml-12 mr-5 mb-[0.8%] phone:-ml-32 phone:mr-0 "
+        data-test-id="img container"
+      >
         <img
           className="w-24 h-24 border-1 rounded-full"
           src={daddy}
