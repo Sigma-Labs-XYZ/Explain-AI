f81a68b47494b0456f7cea5a392d7e69f4af436e
s-dilip <justsyd224@gmail.com>
2023-02-28 15:41:34 +0000

    Added Mobile styling to RelationCard
    Co-Authored-By: yuna-iwata <115478706+yuna-iwata@users.noreply.github.com>

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      phone: { max: "400.5px" },
+      // => @media (min-width: 640px) { ... }
+    },
   },
   plugins: [],
 };
