a47cb62a9e4ef26a8c83893a6dbd8ca2e7273687
s-dilip <justsyd224@gmail.com>
2023-02-28 17:52:45 +0000

    Added tests to see if all elements render correctly

--- web/src/Tests/RelationCard.test.js
+++ web/src/Tests/RelationCard.test.js
@@ -1,5 +1,34 @@
-//plan - write tests the end
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import RelationCard from "../components/RelationCard";
+import { BrowserRouter } from "react-router-dom";
 
-//checking all content is there, title, description, daddy
+describe("Test if all elements of RelationCard are rendered", () => {
+  const name = "HTML";
+  const description = "JavaScript is used to add interactivity to HTML pages";
+  const image = "/image.png";
 
-//Media Queries screen size check for margins
+  test("Name of the topic rendered", () => {
+    render(<RelationCard name={name} description={description} />, {
+      wrapper: BrowserRouter,
+    });
+    expect(screen.getByText(name)).toBeInTheDocument();
+  });
+
+  test("Description of the topic rendered", () => {
+    render(<RelationCard name={name} description={description} />, {
+      wrapper: BrowserRouter,
+    });
+    expect(screen.getByText(description)).toBeInTheDocument();
+  });
+
+  test("Image of the topic rendered", () => {
+    //CHANGE THIS TEST TO HAVE THE ACTUAL VALUE OF IMG SRC NOT PLACEHOLDER DADDY
+    render(<RelationCard name={name} description={description} />, {
+      wrapper: BrowserRouter,
+    });
+    const topicImg = screen.getByRole("img");
+    expect(topicImg).toHaveAttribute("src", "daddy.png");
+    expect(topicImg).toHaveAttribute("alt", "daddy");
+  });
+});
