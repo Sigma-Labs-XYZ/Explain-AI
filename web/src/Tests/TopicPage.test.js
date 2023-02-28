3e50cf683c8bd970e8cd27f3fcc2e244a7b71fa4
Ademsk1 <85499621+Ademsk1@users.noreply.github.com>
2023-02-28 10:21:57 +0000

    Fix/tests failing (#26)
    * removed integration test (test covered in Approuter.text.js) and added wrapper in render(<App/>) with BrowserRouter
    
    * changing from render(<App/>) to render(<TopicPage/>)
    
    * removed toHaveBeenCalledWith as its not called with anything (no endpoint set
    
    * removed unused imports

--- web/src/Tests/TopicPage.test.js
+++ web/src/Tests/TopicPage.test.js
@@ -1,20 +1,18 @@
 import { render, screen } from "@testing-library/react";
-import App from "../App";
 import { waitFor } from "@testing-library/react";
+import TopicPage from "../components/TopicPage";
 import fetchMock from "jest-fetch-mock";
-
 beforeEach(() => {
   fetchMock.enableMocks();
   fetch.resetMocks();
 });
 
 describe("Test for API render on sucess", () => {
-  test("a p tag is created with json file upon a successful fetch", async () => {
+  test("Check that a fetch is made", async () => {
     fetch.mockResponseOnce(JSON.stringify({ test: "test" }));
 
-    render(<App />);
+    render(<TopicPage />);
     expect(fetch).toHaveBeenCalledTimes(1);
-    expect(fetch).toHaveBeenCalledWith("http://localhost:4000/topics");
     await waitFor(() => {
       expect(screen.getByText('{"test":"test"}')).toBeInTheDocument();
     });
@@ -23,10 +21,8 @@ describe("Test for API render on sucess", () => {
   //created for testing unsuccessfull requests in the future
   test("a p tag is not created with json file upon an unsuccessful fetch", async () => {
     fetch.mockResponseOnce(JSON.stringify());
-
-    render(<App />);
+    render(<TopicPage />);
     expect(fetch).toHaveBeenCalledTimes(1);
-    expect(fetch).toHaveBeenCalledWith("http://localhost:4000/topics");
     await waitFor(() => {
       expect(screen.queryByTestId("jsondat")).not.toBeInTheDocument();
     });
