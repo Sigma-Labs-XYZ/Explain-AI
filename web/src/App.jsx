3e9f2c9aa38127de5428e49c2288a4c5eb4f4c9f
s-dilip <93096031+s-dilip@users.noreply.github.com>
2023-02-28 15:17:17 +0000

    Yuna Swastik Header (#30)
    * styled logo in header
    
    * buttons styled
    
    * styled dropdown
    
    Co-authored-by: s-dilip <s-dilip@users.noreply.github.com>
    
    * test commit
    
    * add conditional classes
    
    * local storage added
    
    * local storage and toggle tests
    
    * local storage tests done
    
    * logo test
    
    * Cypress Running
    
    * test elements of header
    
    * Media Queries age selector tests
    
    * Fixed comments and test desc
    
    * Added adult font size change test
    
    * cleared up tests
    
    * Added test for button bg change when clicked
    
    * Added Margin Test for superwide
    
    * More descriptive Test Names
    
    * Snapshot deleted
    
    * Integration test deleted
    
    * Database Commit
    
    * Added setting default age to 5
    
    Co-Authored-By: yuna-iwata <115478706+yuna-iwata@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Yuna Iwata <yunaiwata@Yunas-MacBook-Pro.local>
    Co-authored-by: yuna <yuna.iwata@sigmalabs.co.uk>
    Co-authored-by: s-dilip <s-dilip@users.noreply.github.com>
    Co-authored-by: yuna-iwata <115478706+yuna-iwata@users.noreply.github.com>

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import AppRouter from "./components/AppRouter";
 import "./App.css";
 
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="App">
+      <Header />
       <AppRouter />
     </div>
   );
