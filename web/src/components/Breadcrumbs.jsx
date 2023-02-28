2bb60fbeccef9f24bb0880c32e82a1cbeb43de3e
mdapim <mdapim@outlook.com>
2023-02-28 14:39:14 +0000

    added tailwind

--- web/src/components/Breadcrumbs.jsx
+++ web/src/components/Breadcrumbs.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useState, useEffect, React } from "react";
+import { React } from "react";
 export default function Breadcrumbs({ grandParent, parent }) {
   return (
     <nav>
