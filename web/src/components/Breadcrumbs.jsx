e4137dcf8a8eb44e94b581ca4e3d169c817c55be
bp289 <birajpanta112@gamil.com>
2023-03-01 12:54:58 +0000

    finished scss and tailwind styling for components

--- web/src/components/Breadcrumbs.jsx
+++ web/src/components/Breadcrumbs.jsx
@@ -1,43 +1,43 @@
 import { Link } from "react-router-dom";
 import { React } from "react";
 
-import "../App.css";
+import "../Styles/BreadCrumbs.css";
 export default function Breadcrumbs({ grandParent, parent }) {
   return (
     <>
-      <nav data-testid="desktop">
-        <span className="mr-120 w-screen bg-backdrop text-white capitalize">
+      <nav data-testid="desktop" id="desktop" className="ml-7">
+        <span className=" w-screen bg-backdrop text-white capitalize p-0.5">
           <Link to={`/${grandParent.slug}`}>{grandParent.name} </Link>
           <span data-testid="desktop-grandParent" className="font-thin">
-            >
+            {"> "}
           </span>
           <Link to={`/${parent.slug}`}>{parent.name} </Link>
           <span data-testid="desktop-parent" className="font-thin">
-            {" "}
-            >{" "}
+            {"> "}
           </span>
         </span>
       </nav>
 
-      <div id="mobile" data-testid="mobile" className="text-white capitalize ">
-        <div
-          data-testid="mobile-grandparent"
-          className="bg-backdrop2 pb-2 pt-2 text-left"
-        >
-          <Link className="ml-7" to={`/${grandParent.slug}`}>
-            {grandParent.name}
-          </Link>
-          <span className="font-thin"> > </span>
-        </div>
-        <div
-          data-testid="mobile-parent"
-          className="bg-backdrop pb-2 pt-2 text-left "
-        >
-          <Link className="ml-7" to={`/${parent.slug}`}>
-            {parent.name}
-          </Link>
-          <span className="font-thin"> > </span>
-        </div>
+      <div id="mobile" data-testid="mobile">
+        <Link data-testid="mobile-grandparent" to={`/${grandParent.slug}`}>
+          <button className="bg-backdrop2 pb-2 pt-2 text-left text-white capitalize w-screen">
+            <span className="ml-7">
+              {" "}
+              {grandParent.name}
+              {"  > "}{" "}
+            </span>
+          </button>
+        </Link>
+
+        <Link data-testid="mobile-parent" to={`/${parent.slug}`}>
+          <button className="bg-backdrop pb-2 pt-2 text-left text-white capitalize w-screen">
+            <span className="ml-7">
+              {" "}
+              {parent.name}
+              {"  > "}{" "}
+            </span>
+          </button>
+        </Link>
       </div>
     </>
   );
