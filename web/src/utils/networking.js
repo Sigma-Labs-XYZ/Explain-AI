3183fc498aaac91bdfe54a00e06f30c6bdee4724
Mariam-mohamed110 <mariammohamed0313@gmail.com>
2023-03-02 09:52:21 +0000

    localhost changes 'added changes' t git commit -m 'merged' commit

--- web/src/utils/networking.js
+++ web/src/utils/networking.js
@@ -1,9 +1,11 @@
-export default async function fetchData(url) {
-  try {
-    const apiResponse = await fetch(url);
-    const apiResponseJSON = await apiResponse.json();
-    return apiResponseJSON;
-  } catch (e) {
-    return "unable to fetch data";
-  }
-}
+export const fetchData = async (url) => {
+  // try {
+  console.log(url);
+  const apiResponse = await fetch(url);
+  const apiResponseJSON = await apiResponse.json();
+  return apiResponseJSON;
+  // } catch (e) {
+  // console.log("unable to fetch data", e);
+  return "unable to fetch data";
+  // }
+};
