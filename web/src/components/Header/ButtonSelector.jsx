f1a89215f900125a186faaf818015336c527b41b
s-dilip <justsyd224@gmail.com>
2023-03-02 15:51:17 +0000

    Updated button.jsx added type
    Co-Authored-By: yuna-iwata <115478706+yuna-iwata@users.noreply.github.com>

--- web/src/components/Header/ButtonSelector.jsx
+++ web/src/components/Header/ButtonSelector.jsx
@@ -6,6 +6,8 @@ export default function ButtonSelector({ age, changeAge }) {
     <>
       {["5", "10", "Adult"].map((section) => (
         <button
+          type="button"
+          key={String(section)}
           onClick={() => changeAge(section)}
           className={age === section ? "btn selected" : "btn"}
           children={section}
