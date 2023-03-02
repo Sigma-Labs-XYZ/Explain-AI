a46749c2f78a2599c0112d2b3bbfad3c42a9950c
mdapim <mdapim@outlook.com>
2023-03-02 09:41:57 +0000

    removed console log in util

--- web/src/utils/networking.js
+++ web/src/utils/networking.js
@@ -5,7 +5,6 @@ export const fetchData = async (url) => {
     const apiResponseJSON = await apiResponse.json();
     return apiResponseJSON;
   } catch (e) {
-    console.log("unable to fetch data", e);
     return "unable to fetch data";
   }
 };
