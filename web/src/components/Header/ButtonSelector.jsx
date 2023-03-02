a244f82725ff177b95eeb48e947f979ac8b8fb54
mdapim <mdapim@outlook.com>
2023-03-02 11:56:00 +0000

    finished adding suggestions

--- web/src/components/Header/ButtonSelector.jsx
+++ web/src/components/Header/ButtonSelector.jsx
@@ -4,24 +4,13 @@ import React from "react";
 export default function ButtonSelector({ age, changeAge }) {
   return (
     <>
-      <button
-        onClick={() => changeAge("5")}
-        className={age === "5" ? "btn selected" : "btn"}
-      >
-        5
-      </button>
-      <button
-        onClick={() => changeAge("10")}
-        className={age === "10" ? "btn selected" : "btn"}
-      >
-        10
-      </button>
-      <button
-        onClick={() => changeAge("Adult")}
-        className={age === "Adult" ? "btn selected" : "btn"}
-      >
-        Adult
-      </button>
+      {["5", "10", "Adult"].map((section) => (
+        <button
+          onClick={() => changeAge(section)}
+          className={age === section ? "btn selected" : "btn"}
+          children={section}
+        ></button>
+      ))}
     </>
   );
 }
