7969250645716c0d4f3fc3037cf207896d9ab6ee
Dom Vinyard <domvinyard@me.com>
2023-03-02 10:59:56 +0000

    Update App.js

--- web/src/App.js
+++ web/src/App.js
@@ -3,6 +3,7 @@ import { AppRouter } from "./components/AppRouter";
 import Header from "./components/Header";
 
 function App() {
+  console.log('fake log (would cause netlify to fail before, should not now)')
   return (
     <div className="App">
       <Header />
