eebf58ad89fbdbcb013043b82530a1210f3fa19a
yuna-iwata <115478706+yuna-iwata@users.noreply.github.com>
2023-03-06 16:59:29 +0000

    refactored with useeffect

--- web/src/components/AudienceContext.js
+++ web/src/components/AudienceContext.js
@@ -1,20 +1,20 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 export const ageContext = createContext(null);
 
 export function AudienceContext({ children }) {
-  const DEFAULT_AUDIENCE = 5
+  const DEFAULT_AUDIENCE = 5;
   const [audience, setAudience] = useState(DEFAULT_AUDIENCE);
-  // when page loads, check local-storage for saved state
-  useEffect(() => {
-      const storedAudience = localStorage.getItem("age")
-      if (!storedAudience) updateLocalStorage(DEFAULT_AUDIENCE)
-      else setAudience(storedAudience)
-  }, [])
 
   const updateLocalStorage = (newAge) => {
     localStorage.setItem("age", newAge);
   };
+  // when page loads, check local-storage for saved state
+  useEffect(() => {
+    const storedAudience = localStorage.getItem("age");
+    if (!storedAudience) updateLocalStorage(DEFAULT_AUDIENCE);
+    else setAudience(storedAudience);
+  }, []);
 
   // eslint-disable-next-line
   const value = {
