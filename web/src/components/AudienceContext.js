36dc12f5397a511c7a8d3f04f5de42a8b688a8df
yuna-iwata <115478706+yuna-iwata@users.noreply.github.com>
2023-03-06 16:57:56 +0000

    Update web/src/components/AudienceContext.js
    Co-authored-by: Dom Vin <i@dom.vin>

--- web/src/components/AudienceContext.js
+++ web/src/components/AudienceContext.js
@@ -3,9 +3,14 @@ import React, { createContext, useState } from "react";
 export const ageContext = createContext(null);
 
 export function AudienceContext({ children }) {
-  const [audience, setAudience] = useState(
-    localStorage.getItem("age") || localStorage.setItem("age", "5") || "5",
-  );
+  const DEFAULT_AUDIENCE = 5
+  const [audience, setAudience] = useState(DEFAULT_AUDIENCE);
+  // when page loads, check local-storage for saved state
+  useEffect(() => {
+      const storedAudience = localStorage.getItem("age")
+      if (!storedAudience) updateLocalStorage(DEFAULT_AUDIENCE)
+      else setAudience(storedAudience)
+  }, [])
 
   const updateLocalStorage = (newAge) => {
     localStorage.setItem("age", newAge);
