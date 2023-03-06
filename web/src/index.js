ee26539bf4dfd2fc3221ad7185b601c4eab16b27
s-dilip <justsyd224@gmail.com>
2023-03-06 15:51:46 +0000

    Added sending data for page view
    Co-Authored-By: Biraj <100142251+bp289@users.noreply.github.com>

--- web/src/index.js
+++ web/src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./reportWebVitals"; // eslint-disable-next-line
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-NDPH0VPH2Y");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,7 +17,14 @@ root.render(
   </React.StrictMode>,
 );
 
+const SendAnalytics = () => {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+};
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(SendAnalytics);
