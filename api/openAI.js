5131030ed4231f5436aa920fa41813dd25bf61c2
Dom Vinyard <domvinyard@me.com>
2023-03-06 09:56:09 +0000

    readme

--- api/openAI.js
+++ api/openAI.js
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from "openai";
 import dotenv from "dotenv";
 dotenv.config();
 
-// TODO
+// TODO: See api/README for a list of required queries.
 export const generateTopic = async ({ topicName }) => {
   try {
     const configuration = new Configuration({ apiKey: process.env.OPENAI_KEY });
