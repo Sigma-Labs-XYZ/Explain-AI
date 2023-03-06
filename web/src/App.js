1603105719aa647d4123a936a165eef7e5a26a85
Mariam-mohamed110 <mariammohamed0313@gmail.com>
2023-03-06 12:11:28 +0000

    started use context

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import { React, useState, createContext, useContext } from "react";
 import "./App.css";
 import AppRouter from "./components/AppRouter";
 import Header from "./components/Header/Header";
 
+const ageContext = createContext(null);
+
 function App() {
+  const [age, setAge] = useState(
+    localStorage.getItem("age") || localStorage.setItem("age", "5") || "5",
+  );
+
   return (
     <div className="App">
       <Header />
--- web/src/components/Header/Header.jsx
+++ web/src/components/Header/Header.jsx
@@ -19,6 +19,7 @@ export default function Header() {
   function changeAge(newAge) {
     setAge(newAge);
   }
+
   return (
     <div>
       <header>
