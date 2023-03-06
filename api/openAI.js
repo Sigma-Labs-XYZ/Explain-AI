4b88d5b3f2f53411c955cb5ac3ffabc80e02bd4c
Ademsk1 <85499621+Ademsk1@users.noreply.github.com>
2023-03-06 17:06:32 +0000

    small fix for openAI, please approve asap as this doesnt allow us to use localhost:4000

--- api/openAI.js
+++ api/openAI.js
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // TODO: See api/README for a list of required queries.
-export const generateTopic = async ({ topicName }) => {
+export const generateTopic = async ({ topicName,audience }) => {
+  console.log('audience is here ------------', audience)
   try {
     const configuration = new Configuration({ apiKey: process.env.OPENAI_KEY });
     const openai = new OpenAIApi(configuration);
