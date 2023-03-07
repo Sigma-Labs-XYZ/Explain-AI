a900260945a3e558d794614e891363b1d428b158
Ademsk1 <85499621+Ademsk1@users.noreply.github.com>
2023-03-07 11:52:38 +0000

    adding file

--- web/src/AppRouter/AppRouter.test.js
+++ web/src/AppRouter/AppRouter.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import userEvent from '@testing-library/user-event'
 import "@testing-library/jest-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "../App";
@@ -13,9 +14,27 @@ test("Home Page must be the first Page to be rendered", async () => {
 test("navigating from Home page to Topic Page", async () => {
   render(<App />, { wrapper: BrowserRouter });
   const topic = "javascript";
-  const topicLink = screen.getAllByRole("link")[0];
+  const links = screen.getAllByRole("link");
+  const topicLink = links.filter(link=>link.textContent==='javascript')[0]
   const topicName = topicLink.textContent;
   fireEvent.click(topicLink);
   expect(screen.getByText(topicName)).toBeInTheDocument();
   expect(screen.getByText(topic)).toBeInTheDocument();
 });
+
+  test("the logo should take the user to the homepage when clicked", async ()=>{
+    render(<App />, {wrapper: BrowserRouter});
+    const logo = screen.getByAltText("Logo of explain-ai & link to take you back to the homepage");
+    const links = screen.getAllByRole("link");
+    const topicLink = links.filter(link=>link.textContent==='javascript')[0]
+    const topicName = topicLink.textContent;
+    userEvent.click(topicLink);
+    await waitFor(()=>{
+       expect(global.window.location.pathname).toBe("/javascript");
+    })
+
+    // fireEvent.click(logo)
+    // await waitFor(()=>{
+    //       expect(global.window.location.pathname).toBe("/"); 
+    // })
+  })
