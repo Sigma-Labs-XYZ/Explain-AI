1e16b13864524c0385556c952e2b5c7a1b3146e4
mdapim <115515283+mdapim@users.noreply.github.com>
2023-03-07 17:39:43 +0000

    added button for extend

--- web/src/Pages/TopicPage/TopicCard/TopicCard.js
+++ web/src/Pages/TopicPage/TopicCard/TopicCard.js
@@ -1,14 +1,24 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ageContext } from "../../../components/AudienceContext";
 import "../../../Styling/TopicCard/TopicCard.css";
 
 export default function TopicCard({ topic }) {
   const { audience } = useContext(ageContext);
-
-  const description = topic.descriptions?.find?.(({ audience: a }) => audience === a)?.short;
+  const [descLength, setDescLength] = useState("short");
+  const [buttonTxt, setButtonTxt] = useState("Tell me more");
+  const description = topic.descriptions?.find?.(({ audience: a }) => audience === a)[descLength];
   const imageHandler = () => {
     const imageError = typeof topic.image !== "string" || topic.image === ""; // this may be changed depending on how the API responds
     return imageError ? "./no-image.jpeg" : topic.image;
+  };
+  const lengthSetter = () => {
+    if (descLength === "short") {
+      setDescLength("long");
+      setButtonTxt("Tell me less");
+    } else {
+      setDescLength("short");
+      setButtonTxt("Tell me more");
+    }
   };
   return (
     <div className="topic-card-parent">
@@ -20,7 +30,9 @@ export default function TopicCard({ topic }) {
           <p> {description} </p>
         </div>
         <div className="topic-card-img-btn">
-          <button type="button">Tell me more</button>
+          <button type="button" onClick={lengthSetter}>
+            {buttonTxt}
+          </button>
           <div className="topic-card-img">
             <img src={imageHandler()} alt={`A representation of ${topic.name}`} />
           </div>
