79d444b1400e2464c28cca56992e739fd3d70a89
yuna-iwata <115478706+yuna-iwata@users.noreply.github.com>
2023-03-07 17:59:08 +0000

    added relcard link test

--- web/src/Pages/TopicPage/RelationCard/RelationCard.test.js
+++ web/src/Pages/TopicPage/RelationCard/RelationCard.test.js
@@ -1,14 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, fireEvent } from "react-router-dom";
 import { AudienceContext } from "../../../components/AudienceContext";
 import RelationCard from "./RelationCard";
+import { within } from "@testing-library/dom";
+
+const name = "pedro";
+const description = "JavaScript is used to add interactivity to HTML pages";
+const image = `${process.env.PUBLIC_URL}pedro.png`;
 
 describe("Test if all elements of RelationCard are rendered", () => {
-  const name = "pedro";
-  const description = "JavaScript is used to add interactivity to HTML pages";
-  const image = `${process.env.PUBLIC_URL}pedro.png`;
-
   test("Name of the topic rendered", () => {
     render(
       <AudienceContext>
@@ -43,7 +44,26 @@ describe("Test if all elements of RelationCard are rendered", () => {
       },
     );
     const topicImg = screen.getByRole("img");
-    expect(topicImg).toHaveAttribute("src", "pedro.png");
-    expect(topicImg).toHaveAttribute("alt", "pedro");
+    expect(topicImg).toHaveAttribute("src", `${name}.png`);
+    expect(topicImg).toHaveAttribute("alt", name);
+  });
+});
+
+describe("link elements", () => {
+  test("elements loaded within the link tag so they are all clickable and correct link path", () => {
+    render(
+      <AudienceContext>
+        <RelationCard name={name} description={description} image={image} />
+      </AudienceContext>,
+      {
+        wrapper: BrowserRouter,
+      },
+    );
+    const link = screen.getByRole("link");
+    expect(link).toHaveAttribute("href", `/${name}`);
+    const img = within(link).getByRole("img");
+    expect(img).toHaveAttribute("src", `${name}.png`);
+    const heading = within(link).getByRole("heading");
+    expect(heading.textContent).toBe(name);
   });
 });
