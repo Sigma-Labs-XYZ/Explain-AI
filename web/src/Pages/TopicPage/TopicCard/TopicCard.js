776526c3803933ba51ca673674317a952d4a7104
Ademsk1 <85499621+Ademsk1@users.noreply.github.com>
2023-03-08 14:15:02 +0000

    playing around with animation

--- web/src/Pages/TopicPage/TopicCard/TopicCard.js
+++ web/src/Pages/TopicPage/TopicCard/TopicCard.js
@@ -1,11 +1,16 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState,useEffect } from "react";
 import { ageContext } from "../../../components/AudienceContext";
 import "../../../Styling/TopicCard/TopicCard.css";
+
+const CHARS_PER_LINE = 70; 
+const LINE_HEIGHT = 25;
+
 
 export default function TopicCard({ topic }) {
   const { audience } = useContext(ageContext);
   const [descLength, setDescLength] = useState("short");
   const [buttonTxt, setButtonTxt] = useState("Tell me more");
+  
   const description = topic.descriptions?.find?.(({ audience: a }) => audience === a)[descLength];
   const imageHandler = () => {
     const imageError = typeof topic.image !== "string" || topic.image === ""; // this may be changed depending on how the API responds
@@ -20,13 +25,24 @@ export default function TopicCard({ topic }) {
       setButtonTxt("Tell me more");
     }
   };
+  const heightSetter = () => {
+    const stringLength = description.length;
+    const numberOfLines = Math.ceil(stringLength/CHARS_PER_LINE);
+    const pixelHeight =  numberOfLines * LINE_HEIGHT;
+    return pixelHeight
+  }
+  const [topicHeight,setTopicHeight] = useState(heightSetter())
+  useEffect(()=> {
+    setTopicHeight(heightSetter())
+  },[buttonTxt])
+
   return (
     <div className="topic-card-parent">
       <div className="topic-card">
         <div className="topic-card-title">
           <h1>{topic.name}</h1>
         </div>
-        <div className={`topic-card-description ${descLength === "long" ? "expanding" : ""}`}>
+        <div style={{"max-height":`${heightSetter()}px`}} className={`topic-card-description ${descLength === "long" ? "expanding" : ""}`}>
           <p data-testid="description-container" className="topic-desc">
             {" "}
             {description}{" "}
