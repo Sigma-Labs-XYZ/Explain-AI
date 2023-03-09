5eaa79223157b95edd872e8f4b458efabab3d050
Ademsk1 <85499621+Ademsk1@users.noreply.github.com>
2023-03-09 14:19:13 +0000

    fixed prettier issues

--- api/openAI.js
+++ api/openAI.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // TODO: See api/README for a list of required queries.
-export const generateTopic = async ({ topicName,audience }) => {
+export const generateTopic = async ({ topicName, audience }) => {
   try {
     const configuration = new Configuration({ apiKey: process.env.OPENAI_KEY });
     const openai = new OpenAIApi(configuration);
