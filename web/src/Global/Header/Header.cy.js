034ee98d06ecc40d7892e5d24d07b022cf87f08d
mdapim <115515283+mdapim@users.noreply.github.com>
2023-03-09 13:15:24 +0000

    Cypress adding audience and router wrappers (#95)
    * added audience context and browser router to tests, changed a get function in header
    
    * added cypress action to yml
    
    * merged with main
    
    * added dotconfig to jest setup
    
    * added .env to cypress yaml
    
    * got rid of jest setup in package.json
    
    * added margin change to allow test to pass

--- web/src/Global/Header/Header.cy.js
+++ web/src/Global/Header/Header.cy.js
@@ -1,48 +1,92 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import AudienceContext from "../../components/AudienceContext";
 import Header from "./Header";
 
 describe("Media queries and their effects on Age Toggle and Dropdown", () => {
   it("Toggle appears when screen width above 400px (Desktop)", () => {
     cy.viewport(405, 1000);
-    cy.mount(<Header />);
+    cy.mount(
+      <AudienceContext>
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
+      </AudienceContext>,
+    );
     cy.get("button").should("not.have.css", "display", "none");
   });
 
   it("Toggle disappears when screen width below 400px (Mobile)", () => {
     cy.viewport(395, 1000);
-    cy.mount(<Header />);
+    cy.mount(
+      <AudienceContext>
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
+      </AudienceContext>,
+    );
     cy.get("button").should("have.css", "display", "none");
   });
 
   it("Dropdown appears when screen width below 400px (Desktop)", () => {
     cy.viewport(395, 1000);
-    cy.mount(<Header />);
+    cy.mount(
+      <AudienceContext>
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
+      </AudienceContext>,
+    );
     cy.get("select").should("not.have.css", "display", "none");
   });
 
   it("Dropdown disappears when screen width above 400px (Mobile)", () => {
     cy.viewport(405, 1000);
-    cy.mount(<Header />);
+    cy.mount(
+      <AudienceContext>
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
+      </AudienceContext>,
+    );
     cy.get("select").should("have.css", "display", "none");
   });
 
   it("15% Margin appears on both sides of Elements when screen width above 1000px (Super-wide Desktop)", () => {
     cy.viewport(1200, 1000);
-    cy.mount(<Header />);
-    cy.get("img").should("have.css", "margin-left", "177.594px");
+    cy.mount(
+      <AudienceContext>
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
+      </AudienceContext>,
+    );
+    cy.get('[data-test-id="router-Link"]').should("have.css", "margin-left", "180px");
   });
 });
 
 it("Selecting Adult in dropdown reduces font size to 10px", () => {
   cy.viewport(395, 1000);
-  cy.mount(<Header />);
+  cy.mount(
+    <AudienceContext>
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    </AudienceContext>,
+  );
   cy.get("select").invoke("val", "Adult").trigger("change");
   cy.get("select").should("have.css", "font-size", "18px");
 });
 
 it("Background color changes when a button is clicked", () => {
   cy.viewport(405, 1000);
-  cy.mount(<Header />);
+  cy.mount(
+    <AudienceContext>
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    </AudienceContext>,
+  );
   cy.get("button")
     .contains("10")
     .should("have.css", "background-color", "rgb(50, 50, 50)")
