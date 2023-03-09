3f164c3a9af868a3df9b54b35c1b07ceb94e98a0
Dom Vinyard <domvinyard@me.com>
2023-03-09 14:40:40 +0000

    db endpoint

--- api/generate.js
+++ api/generate.js
@@ -40,6 +40,21 @@ const saveToDatabase = async ({ slug, name, data, result }) => {
 
   // save data object as json file in /test directory
   fs.writeFileSync(`./test/${slug}.json`, JSON.stringify(result));
+
+//   {"topics": [
+//   {"name": "Test 1", "slug": "test_1"},
+//   {"name": "Test 2", "slug": "test_2"}
+// ], 
+// "descriptions": [
+//   {"topic_slug": "test_1", "audience": 5, "short": "test test", "extra_short": "test", "long": "test test test"}
+// ],
+// "relationships": [
+//   {"from_slug": "test_1", "to_slug": "test_2", "description": "test", "audience": 5, "priority": 0}
+// ],
+//   "hierarchies": [
+//     {"parent_slug": "test_1", "child_slug": "test_2"}
+//   ]
+// }
 };
 
 const trim = (item) =>
