a0b490e114680949f0d4a5e75dac0389e7d64f4a
mdapim <115515283+mdapim@users.noreply.github.com>
2023-03-10 15:04:10 +0000

    added homepagecard jsx

---
+++ web/src/Pages/HomePage/HomePageCard.js/HomePageCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import RelationCard from "../../TopicPage/RelationCard/RelationCard";
+
+export default function HomePageCard() {
+  return (
+    <div>
+      <h1> Topic </h1>
+    </div>
+  );
+}
