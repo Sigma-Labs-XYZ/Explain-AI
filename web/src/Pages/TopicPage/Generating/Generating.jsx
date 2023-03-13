e66ea1b4f7b6a145cec9a4ce509a42c60f70ccf6
bp289 <birajpanta112@gamil.com>
2023-03-13 15:56:57 +0000

    testing generating component

--- web/src/Pages/TopicPage/Generating/Generating.jsx
+++ web/src/Pages/TopicPage/Generating/Generating.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 // eslint-disable-next-line import/no-extraneous-dependencies
 import Lottie from "lottie-react";
 import GeneratingAnimation from "./GeneratingAnimation.json";
@@ -7,7 +8,8 @@ import "../../../Styling/Generating/generatingPage.css";
 export default function Generating({ topic }) {
   return (
     <div className="generating">
-      <h1 className="title">Generating</h1>
+      <img className="img" src={topic.image || "/no-image.jpeg"} alt={topic.name} />
+      <h1 className="title">Generating {topic.name}</h1>
       <p className="description">
         You are the first person to generate this topic! Please wait for a description to be
         generated.
