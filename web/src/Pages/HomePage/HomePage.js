0020258be7c22fbec6a22bdb0cb4f38c444d8e9e
yuna-iwata <115478706+yuna-iwata@users.noreply.github.com>
2023-03-13 14:11:59 +0000

    react helmet async

--- web/src/Pages/HomePage/HomePage.js
+++ web/src/Pages/HomePage/HomePage.js
@@ -1,21 +1,24 @@
 import React from "react";
 // eslint-disable-next-line
-import { Helmet } from "react-helmet";
+import Helmet, { HelmetProvider } from "react-helmet-async";
+// import { Helmet } from "react-helmet";
 
 export default function HomePage() {
   return (
-    <div>
-      <Helmet>
-        {/* <title>My Title</title> */}
-        <title>Explain AI</title>
-        <meta
-          property="og:description"
-          content="Explain it to me. Simply."
-          data-react-helmet="true"
-        />
-      </Helmet>
-      <h1>Homepage</h1>
-      <a href="/javascript">javascript</a>
-    </div>
+    <HelmetProvider>
+      <div>
+        <Helmet>
+          {/* <title>My Title</title> */}
+          <title>Explain AI</title>
+          <meta
+            property="og:description"
+            content="Explain it to me. Simply."
+            data-react-helmet="true"
+          />
+        </Helmet>
+        <h1>Homepage</h1>
+        <a href="/javascript">javascript</a>
+      </div>
+    </HelmetProvider>
   );
 }
