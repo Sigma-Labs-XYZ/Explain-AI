70da1534fe1fc24c80bc4c372f8a1be6af70e9e8
Dom Vinyard <domvinyard@me.com>
2023-03-14 09:54:09 +0000

    Update networking.js

--- web/src/utils/networking.js
+++ web/src/utils/networking.js
@@ -26,7 +26,7 @@ export const postData = async ({ url, body }) => {
 
 export async function homepageFetch() {
   try {
-    const response = await fetch(`${process.env.REACT_APP_GROUPS_URL}`);
+    const response = await fetch(`${process.env.REACT_APP_API_ENDPOINT}/groups`);
     const data = await response.json();
     return data;
   } catch (e) {
