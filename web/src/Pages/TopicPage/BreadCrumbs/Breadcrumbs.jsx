7efc02dc76ee107b2dfa805ca1550e2c791fcd22
yuna-iwata <115478706+yuna-iwata@users.noreply.github.com>
2023-03-15 10:53:54 +0000

    breadcrumb works without parent or grandparent (#130)

--- web/src/Pages/TopicPage/BreadCrumbs/Breadcrumbs.jsx
+++ web/src/Pages/TopicPage/BreadCrumbs/Breadcrumbs.jsx
@@ -1,73 +1,98 @@
 /* eslint-disable react/button-has-type */
 import { Link } from "react-router-dom";
-import { React } from "react";
+import { React, useEffect, useState } from "react";
 import PropType from "prop-types";
 import "../../../Styling/TopicBreadcrumbs/TopicBreadcrumbs.css";
 import { sendBreadCrumbEvent } from "../../../utils/gaEvents";
 
 function Breadcrumbs({ grandParent, parent, current }) {
+  const [dad, setDad] = useState(parent);
+  const [grandad, setGrandad] = useState(grandParent);
+  useEffect(() => {
+    if (!parent) {
+      setDad({
+        name: "",
+        slug: current,
+      });
+      setGrandad({
+        name: "",
+        slug: current,
+      });
+    } else if (!grandParent) {
+      setGrandad({
+        name: "",
+        slug: current,
+      });
+    }
+  }, []);
+
   return (
     <div className="bg-backdrop h-[65px]">
-      <nav data-testid="desktop" id="desktop" className="desktop">
-        <span className="desktop-container">
+      {grandad && dad ? (
+        <nav data-testid="desktop" id="desktop" className="desktop">
+          <span className="desktop-container">
+            <Link
+              onClick={() => {
+                sendBreadCrumbEvent(current, grandad.name);
+              }}
+              to={`/${grandad.slug}`}
+            >
+              {grandad.name}
+            </Link>
+            <span data-testid="desktop-grandParent" className="font-thin">
+              {" > "}
+            </span>
+
+            <Link
+              onClick={() => {
+                sendBreadCrumbEvent(current, dad.name);
+              }}
+              to={`/${dad?.slug}`}
+            >
+              {dad.name}
+            </Link>
+            <span data-testid="desktop-parent" className="font-thin">
+              {" > "}
+            </span>
+          </span>
+        </nav>
+      ) : null}
+
+      {grandad && dad ? (
+        <div id="mobile" data-testid="mobile">
           <Link
             onClick={() => {
-              sendBreadCrumbEvent(current, grandParent.name);
+              sendBreadCrumbEvent(current, grandad.name);
             }}
-            to={`/${grandParent.slug}`}
+            data-testid="mobile-grandParent"
+            to={`/${grandad.slug}`}
           >
-            {grandParent.name}
+            <button className="mobile-parent-btn">
+              <span className="ml-7">
+                {" "}
+                {grandad.name}
+                {"  > "}{" "}
+              </span>
+            </button>
           </Link>
-          <span data-testid="desktop-grandParent" className="font-thin">
-            {" > "}
-          </span>
+
           <Link
             onClick={() => {
-              sendBreadCrumbEvent(current, parent.name);
+              sendBreadCrumbEvent(current, dad.name);
             }}
-            to={`/${parent.slug}`}
+            data-testid="mobile-parent"
+            to={`/${dad.slug}`}
           >
-            {parent.name}
+            <button className="mobile-grandParent-btn">
+              <span className="ml-7">
+                {" "}
+                {dad.name}
+                {"  > "}{" "}
+              </span>
+            </button>
           </Link>
-          <span data-testid="desktop-parent" className="font-thin">
-            {" > "}
-          </span>
-        </span>
-      </nav>
-
-      <div id="mobile" data-testid="mobile">
-        <Link
-          onClick={() => {
-            sendBreadCrumbEvent(current, grandParent.name);
-          }}
-          data-testid="mobile-grandParent"
-          to={`/${grandParent.slug}`}
-        >
-          <button className="mobile-parent-btn">
-            <span className="ml-7">
-              {" "}
-              {grandParent.name}
-              {"  > "}{" "}
-            </span>
-          </button>
-        </Link>
-
-        <Link
-          onClick={() => {
-            sendBreadCrumbEvent(current, parent.name);
-          }}
-          data-testid="mobile-parent"
-          to={`/${parent.slug}`}
-        >
-          <button className="mobile-grandParent-btn">
-            <span className="ml-7">
-              {" "}
-              {parent.name}
-              {"  > "}{" "}
-            </span>
-          </button>
-        </Link>
-      </div>
+        </div>
+      ) : null}
     </div>
   );
 }
